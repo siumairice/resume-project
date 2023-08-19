019d83303862248d8112096c98f0552d4ffa47e6
Akshaya Varakunan <139722405+ladybugv@users.noreply.github.com>
2023-08-19 00:28:38 -0400

    basic design for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
-import LandingPage from './components/LandingPage'
+import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-
   return (
     <>
-      <LandingPage/>
+      <LandingPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

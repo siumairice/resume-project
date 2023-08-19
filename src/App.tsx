7f5ab5ccf889997f8d7d277c89d3a9d605e03f31
Akshaya Varakunan <139722405+ladybugv@users.noreply.github.com>
2023-08-19 05:36:03 -0400

    created experience component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
+import Experience from "./components/Experience";
 import LandingPage from "./components/LandingPage";
+import SkillButtons from "./components/SkillButtons";
 
 function App() {
   return (
